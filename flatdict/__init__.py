5e051d74a1d95c2e8f276cda73dc7fe72ed45308
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-29 15:13:56 +0100

    first implementation

--- flatdict/__init__.py
+++ flatdict/__init__.py
@@ -1,5 +1,16 @@
 # -*- coding: UTF-8 -*-
 
 
-def flatdict(d):
-    pass
+def flatdict(d): # TESTME
+    if type(d) is not dict:
+        return d
+
+    flat = {}
+    for k, v in d.items():
+        if type(v) is dict:
+            d2 = flatdict(v)
+            for k2, v2 in d2.items():
+                flat["%s.%s" % (k, k2)] = flatdict(v2)
+        else:
+            flat[k] = v
+    return flat
