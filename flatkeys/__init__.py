7d189ff1124a05a4b4adf9869c911991e7e9c983
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-31 00:31:15 +0100

    doc added

--- flatkeys/__init__.py
+++ flatkeys/__init__.py
@@ -4,6 +4,20 @@ __version__ = '0.0.1'
 
 
 def flatkeys(d, sep="."):
+    """
+    Flatten a dictionary: build a new dictionary from a given one where all
+    non-dict values are left untouched but nested ``dict``s are recursively
+    merged in the new one with their keys prefixed by their parent key.
+
+    >>> flatkeys({1: 42, 'foo': 12})
+    {1: 42, 'foo': 12}
+    >>> flatkeys({1: 42, 'foo': 12, 'bar': {'qux': True}})
+    {1: 42, 'foo': 12, 'bar.qux': True}
+    >>> flatkeys({1: {2: {3: 4}}})
+    {'1.2.3': 4}
+    >>> flatkeys({1: {2: {3: 4}, 5: 6}})
+    {'1.2.3': 4, '1.5': 6}
+    """
     flat = {}
     dicts = [("", d)]
 
